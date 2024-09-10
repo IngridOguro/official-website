56bbdccd4914010a98dd77095ca6d5d358a92c32
IS Oguro <ingrid.shizue@gmail.com>
2024-09-09 23:56:10 -0300

    Estrutura inicial + header

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import './header.css';
+
+export default function Header(){
+    return (
+      <div className='header'>
+            <a class="header-link" href="/">SOBRE</a>
+            <a class="header-link" href="/">CAMPANHAS</a>
+            <a class="header-link" href="/">EQUIPES</a>
+            <a class="header-link" href="/">BLOG</a>
+            <a class="header-link" href="/">CONTATO</a>
+    </div>
+    );
+  }
---
+++ src/pages/homepage/home.js
@@ -0,0 +1,8 @@
+import './home.css';
+import Header from '../../components/header/header';
+
+export default function Home(){
+    return (
+      <Header/>
+    );
+  }
