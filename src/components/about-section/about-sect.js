d1aaf3fd605cb2995b6f1fd1227cd3511e0a3767
IS Oguro <ingrid.shizue@gmail.com>
2024-09-10 00:55:38 -0300

    seção sobre

---
+++ src/components/about-section/about-sect.js
@@ -0,0 +1,11 @@
+import './about-section.css';
+import aboutimg from '../../images/about-img.png';
+
+export default function aboutSection(){
+    return (
+      <div class="about-section">
+        <img src={aboutimg} alt="aboutimg" class="tgtrgtgimg"/>
+        <a>a</a>
+      </div>
+    );
+  }
