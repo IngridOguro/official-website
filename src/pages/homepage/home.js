b7fc325bb1707f65fb289be4ec9d835fd41c6118
IS Oguro <ingrid.shizue@gmail.com>
2024-11-02 13:12:38 -0300

    Alterando home

--- src/pages/homepage/home.js
+++ src/pages/homepage/home.js
@@ -1,14 +1,13 @@
 import './home.css';
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
-import aboutSection from '../../components/about-section/about-sect';
+import QuemSomos from '../../components/quem-somos/quem-somos'
 
 export default function Home(){
     return (
       <div class="home">
         <Header/>
-        <aboutSection/>
-        <Footer/>
+        <QuemSomos/>
       </div>
     );
   }
