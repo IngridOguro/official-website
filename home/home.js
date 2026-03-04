a1c2d142df2873f17c27133ba0ab405652039f1e
IS Oguro <ingrid.shizue@gmail.com>
2026-03-04 20:35:25 -0300

    link pagina sobre

--- home/home.js
+++ home/home.js
@@ -1,12 +1,12 @@
-// import { Navbar } from '../../components/Navbar/Navbar.js';
 import { Header } from '../../components/Header/Header.js';
 import {Banner} from '../home/sections/banner/banner.js';
 
 const links = [
+  { label: 'SOBRE', href: '/sobre' },
   { label: 'QUEM SOMOS', href: '#quem-somos' },
   { label: 'IMPACTO', href: '#impacto' },
-  { label: 'NA MIDIA', href: '#naMidia' },
-  { label: 'CONTATO', href: '#contato' }
+  { label: 'NA MIDIA', href: '#naMidia' }
+
 ];
 
 document.body.insertAdjacentHTML('afterbegin', Header(links));
