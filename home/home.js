115cb3661d12c84402eacc12b3a483a33803b934
IS Oguro <ingrid.shizue@gmail.com>
2026-03-04 21:31:13 -0300

    benfeitoria

--- home/home.js
+++ home/home.js
@@ -1,15 +1,15 @@
 import { Header } from '../../components/Header/Header.js';
-import {Banner} from '../home/sections/banner/banner.js';
+import { Banner } from '../home/sections/banner/banner.js';
 import { Footer } from '../components/Footer/Footer.js';
 import { QuemSomos } from './sections/quem-somos/quem-somos.js';
 import { WorldPeaceForum } from './sections/world-peace-forum/world-peace-forum.js';
+import { Benfeitoria } from './sections/benfeitoria/benfeitoria.js';
 
 const links = [
   { label: 'SOBRE', href: '/sobre' },
   { label: 'QUEM SOMOS', href: '#quem-somos' },
   { label: 'IMPACTO', href: '#impacto' },
   { label: 'NA MIDIA', href: '#naMidia' }
-
 ];
 
 document.body.insertAdjacentHTML('afterbegin', Header(links));
@@ -20,5 +20,6 @@ main.innerHTML = `
   ${Banner()}
   ${QuemSomos()}
   ${WorldPeaceForum()}
+  ${Benfeitoria()}
   ${Footer()}
 `;
---
+++ home/sections/benfeitoria/benfeitoria.js
@@ -0,0 +1,24 @@
+export function Benfeitoria(links = []) {
+  return `
+          <section id="bemfeitoria" class="d-flex flex-column w-100">
+            <img src="assets/images/medalha.png" alt="" class="img">
+            <p class="text-center">
+                <strong>
+                    AHTP
+                </strong>
+                é vencedor na Categoria Ambiental
+                <br>
+                no Prêmio da
+                <strong>
+                    Benfeitoria
+                </strong>
+            </p>
+            <a href="https://premio.benfeitoria.com/vencedores.html#ambiental" target="_blank"
+                rel="noopener noreferrer">
+                <button>
+                    leia mais
+                </button>
+            </a>
+        </section>
+  `;
+}
