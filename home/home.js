a3a17ecff743167dac1cf290f0731a2029c6f5bf
IS Oguro <ingrid.shizue@gmail.com>
2026-03-04 21:13:57 -0300

    seção quem-somos

--- home/home.js
+++ home/home.js
@@ -1,6 +1,7 @@
 import { Header } from '../../components/Header/Header.js';
 import {Banner} from '../home/sections/banner/banner.js';
 import { Footer } from '../components/Footer/Footer.js';
+import { QuemSomos } from './sections/quem-somos/quem-somos.js';
 
 const links = [
   { label: 'SOBRE', href: '/sobre' },
@@ -16,5 +17,6 @@ const main = document.querySelector('main');
 
 main.innerHTML = `
   ${Banner()}
+  ${QuemSomos()}
   ${Footer()}
 `;
