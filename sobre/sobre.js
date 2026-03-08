5666546419432bcf9099e7f4b316539bbbd0b8cc
IS Oguro <ingrid.shizue@gmail.com>
2026-03-08 12:34:52 -0300

    removendo import

--- sobre/sobre.js
+++ sobre/sobre.js
@@ -1,5 +1,5 @@
 import { Header } from '../../components/Header/Header.js';
-import { Footer } from '../components/Footer/Footer.js';
+// import { Footer } from '../components/Footer/Footer.js';
 import { SobreContent } from './sobre-content/sobre-content.js';
 import { SobreMobileSlide } from './sobre-mobile-slide/sobre-mobile-slide.js';
 
