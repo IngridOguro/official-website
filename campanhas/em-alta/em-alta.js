c965c791a138b107420c9d40549d5cd09fe0ed5b
IS Oguro <ingrid.shizue@gmail.com>
2026-03-17 21:31:41 -0300

    componente em-alta recebe informação das campanhas dinamicamente

--- campanhas/em-alta/em-alta.js
+++ campanhas/em-alta/em-alta.js
@@ -1,38 +1,36 @@
-export function EmAlta() {
-  return `
+import { getCampanhas } from "../service/campanhasService.js";
+
+export async function EmAlta() {
+  const campanhas_json = await getCampanhas();
+
+  const campanhas = campanhas_json.map(c => ({
+    titulo: c.titulo,
+    imagem: c.imagem
+  }));
+
+    return `
           <div id="campanhas-titulo">
             <a id="campanhas-titulo-texto">CAMPANHAS</a>
           </div>
+
           <div id="em-alta">
             <div class="em-alta-titulo">
               <a>EM ALTA</a>
             </div>
             <div class="em-alta-cards">
+            ${campanhas.map(campanha => `
               <div class="em-alta-card">
                 <div class="em-alta-card-conteudo">
+                <img src="${campanha.imagem}" alt="">
                 </div>
                 <div class="em-alta-card-label">
-                  <a>Help the Pantanal</a>
+                  <a> ${campanha.titulo}</a>
                 </div>
               </div>
-
-              <div class="em-alta-card">
-                <div class="em-alta-card-conteudo">
-                </div>
-                <div class="em-alta-card-label">
-                  <a>Help the Pantanal</a>
-                </div>
-              </div>
-
-              <div class="em-alta-card">
-                <div class="em-alta-card-conteudo">
-                </div>
-                <div class="em-alta-card-label">
-                  <a>Help the Pantanal</a>
-                </div>
-              </div>
-
+            `).join('')}
             </div>
           </div>
   `;
+
 }
+
