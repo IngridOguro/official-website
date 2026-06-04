6c0ca0b4bb28cc4a48e85e2ce1c426f5feb89ddf
IS Oguro <ingrid.shizue@gmail.com>
2026-06-04 17:30:51 -0300

    abrir modal, fechar modal

--- campanhas/campanhas.js
+++ campanhas/campanhas.js
@@ -19,7 +19,53 @@ async function init() {
     ${await Historico()}
     ${Footer()}
   `;
+
+  // ABRIR
+  document.querySelectorAll(".abrirModal").forEach(card => {
+
+    card.addEventListener("click", () => {
+
+      const modal = card.querySelector(".modal");
+
+      modal.style.display = "flex";
+    });
+
+  });
+
+  // FECHAR NO X
+  document.querySelectorAll(".fecharModal").forEach(botao => {
+
+    botao.addEventListener("click", (event) => {
+
+      event.stopPropagation();
+
+      const modal = botao.closest(".modal");
+
+      modal.style.display = "none";
+    });
+
+  });
+
+  // FECHAR CLICANDO FORA
+  document.querySelectorAll(".modal").forEach(modal => {
+
+    modal.addEventListener("click", () => {
+  event.stopPropagation();
+      modal.style.display = "none";
+    });
+
+  });
+
+  // IMPEDIR FECHAR AO CLICAR DENTRO
+  document.querySelectorAll(".modal-conteudo").forEach(conteudo => {
+
+    conteudo.addEventListener("click", (event) => {
+
+      event.stopPropagation();
+
+    });
+
+  });
 }
 
 init();
-
