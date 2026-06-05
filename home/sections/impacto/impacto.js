83614f25ac0c52e54689acb5bc87e075cecf5384
IS Oguro <ingrid.shizue@gmail.com>
2026-06-04 21:18:54 -0300

    adicionando simbolos a seção impacto da home

--- home/sections/impacto/impacto.js
+++ home/sections/impacto/impacto.js
@@ -3,18 +3,21 @@ export function Impacto() {
           <div id="impacto" class="numeros section">
             <div class="numeros-cards-div">
                 <div class="numeros-cards">
+                    <a class="numero-cards-simbolo">R$</a>
                     <a class="numero-cards-valor">202.290</a>
                     <a class="numero-cards-desc">
                         Arrecadados em
                         <br> nossas campanhas</a>
                 </div>
                 <div class="numeros-cards">
+                    <a class="numero-cards-simbolo">+</a>
                     <a class="numero-cards-valor">4.900</a>
                     <a class="numero-cards-desc">Doadores contribuindo
                         <br> com o melhor do ARMY
                         <br> e do BTS​</a>
                 </div>
                 <div class="numeros-cards">
+                    <a class="numero-cards-simbolo">+</a>
                     <a class="numero-cards-valor">30</a>
                     <a class="numero-cards-desc">Voluntários pelo Brasil​</a>
                 </div>
